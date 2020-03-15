52e614342053567242d8f86776d228f7dce31265
Mrklata <lukasz98kowalczyk@gmail.com>
2020-03-15 13:08:18 +0100

    Changing background image link

--- accounts/models.py
+++ accounts/models.py
@@ -7,7 +7,7 @@ class LangDescription(models.Model):
                 ('Django', 'Django'),
                 ('CSS', 'CSS'),
                 )
-    selection = models.CharField(max_length=100, choices=LANGUAGE, default='Python')
+    selection = models.CharField(max_length=100, choices=LANGUAGE, default='HTML')
     name = models.CharField(max_length=100)
     description = models.TextField(help_text='skill description')
     libs = models.TextField(help_text='name libraries', default='Some of libraries')
